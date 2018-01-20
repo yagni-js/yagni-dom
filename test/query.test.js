b2cf66edf8d5093678baa95d5bc73528438d47c1
Yuri Egorov <ysegorov@gmail.com>
2018-01-20 22:10:43 +0300

    + add pending tests for query() and queryFirst() functions

--- test/query.test.js
+++ test/query.test.js
@@ -105,21 +105,138 @@ describe('closest()', function () {
 
 describe('query()', function () {
 
-  it('should return proper element by id');
+  const root = window.document.body;
+  const render = dom.render(root);
 
-  it('should return proper elements by single class');
+  before(function () {
 
-  it('should return proper elements by multiple classes');
+    const tree = dom.h('div', {id: 'body'}, {}, [
+      dom.h('div', {id: 'header'}, {}, ['Header']),
+      dom.h('div', {id: 'content', 'class': 'foo baz bar'}, {}, ['Content']),
+      dom.h('div', {id: 'footer', 'data-foo': '42'}, {}, ['Footer']),
+      dom.h('div', {id: 'popup'}, {}, [
+        dom.h('p', {}, {}, [
+          dom.h('span', {}, {}, ['popup']),
+          dom.h('span', {}, {}, ['popup'])
+        ]),
+      ])
+    ]);
 
-  it('should return proper elements by tag name');
+    const body = render(tree);
 
-  it('should return proper elements by attribute');
+  });
+
+  after(function () {
+
+    dom.removeChildren(root);
+
+  });
+
+  it('should return proper element by id', function () {
+
+    const getHeader = dom.query('#header');
+    const header = getHeader(root);
+
+    expect(header).to.be.a('htmldivelement');
+    expect(dom.textContent(header)).to.equal('Header');
+
+  });
+
+  it('should return proper elements by single class', function () {
+
+    const getId = dom.getProp('id');
+    const getBaz = dom.query('.baz');
+    const baz = getBaz(root);
+
+    expect(baz).to.be.an('array');
+    expect(baz).to.have.length(1);
+    expect(baz[0]).to.be.a('htmldivelement');
+    expect(dom.textContent(baz[0])).to.equal('Content');
+    expect(getId(dom.parent(baz[0]))).to.equal('body');
+
+  });
+
+  it('should return proper elements by multiple classes', function () {
+
+    const getId = dom.getProp('id');
+    const getFooBazBar = dom.query('.foo.baz.bar');
+    const fooBazBar = getFooBazBar(root);
+
+    expect(fooBazBar).to.be.an('array');
+    expect(fooBazBar).to.have.length(1);
+    expect(fooBazBar[0]).to.be.a('htmldivelement');
+    expect(dom.textContent(fooBazBar[0])).to.equal('Content');
+    expect(getId(dom.parent(fooBazBar[0]))).to.equal('body');
+
+  });
+
+  it('should return proper elements by tag name', function () {
+
+    const getSpan = dom.query('span');
+    const getP = dom.query('p');
+
+    const span = getSpan(root);
+    const p = getP(root);
+
+    expect(span).to.be.an('array');
+    expect(span).to.have.length(2);
+    expect(dom.textContent(span[0])).to.equal('popup');
+    expect(dom.textContent(span[1])).to.equal('popup');
+
+    expect(p).to.be.an('array');
+    expect(p).to.have.length(1);
+    expect(dom.textContent(p[0])).to.equal('popuppopup');
+
+  });
+
+  it('should return proper elements by attribute', function () {
+
+    const getFoo = dom.query('[data-foo="42"]');
+    const foo = getFoo(root);
+
+    expect(foo).to.be.an('array');
+    expect(foo).to.have.length(1);
+    expect(foo[0]).to.be.a('htmldivelement');
+    expect(dom.textContent(foo[0])).to.equal('Footer');
+
+  });
 
 });
 
 
 describe('queryFirst()', function () {
 
-  it('should return first found element');
+  const root = window.document.body;
+  const render = dom.render(root);
+
+  before(function () {
+
+    const tree = dom.h('div', {id: 'body'}, {}, [
+        dom.h('p', {}, {}, [
+          dom.h('span', {}, {}, ['foo']),
+          dom.h('span', {}, {}, ['baz'])
+      ])
+    ]);
+
+    const body = render(tree);
+
+  });
+
+  after(function () {
+
+    dom.removeChildren(root);
+
+  });
+
+  it('should return first found element', function () {
+
+    const getSpan = dom.queryFirst('span');
+
+    const span = getSpan(root);
+
+    expect(span).to.be.a('htmlspanelement');
+    expect(dom.textContent(span)).to.equal('foo');
+
+  });
 
 });
