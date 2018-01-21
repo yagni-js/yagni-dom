bbcb0c4c4026d71fb2f9b3626891570c9d4f1de0
Yuri Egorov <ysegorov@gmail.com>
2018-01-21 21:14:15 +0300

    + add tests for inputType(), serializeInput() and serializeForm() functions, fix radio input serialization

--- src/form.js
+++ src/form.js
@@ -1,5 +1,5 @@
 
-import {always, call, callMethod, equals, identity, ifElse, isNil, obj, or, pipe, reduce } from 'yagni';
+import {always, call, callMethod, equals, fn2, identity, ifElse, isNil, obj, or, pipe, reduce } from 'yagni';
 
 import { query } from './query.js';
 import { getProp } from './props.js';
@@ -7,39 +7,51 @@ import { getProp } from './props.js';
 
 const inputName = getProp('name');
 const inputValue = getProp('value');
-const inputTypeProperty = getProp('type');
+export const inputType = getProp('type');
 
 const isChecked = getProp('checked');
 const isDisabled = getProp('disabled');
 
-export const inputType = pipe([
-  inputTypeProperty,
-  ifElse(
-    isNil,
-    always('text'),
-    callMethod(identity, 'toLowerCase')
-  )
-]);
-
-const isFile = equals('file');
-const isCheckbox = equals('checkbox');
-const isRadio = equals('radio');
-const isBoolean = pipe([
+const isFile = pipe([
   inputType,
-  or(isCheckbox, isRadio)
+  equals('file')
+]);
+const isCheckbox = pipe([
+  inputType,
+  equals('checkbox')
+]);
+const isRadio = pipe([
+  inputType,
+  equals('radio')
 ]);
 
-export function serializeInput(el) {
-  // TODO use pipe + transform + ifElse?
-  const isBooleanInput = isBoolean(el);
-  const isFileInput = isFile(el);
-  const isInputDisabled = isDisabled(el);
-
-  const name = inputName(el);
-  const value = isBooleanInput ? isChecked(el) : inputValue(el);
-
-  return isInputDisabled || isFileInput || (isBooleanInput && !value) ? {} : obj(name, value);
+function serializeRadio(el) {
+  return isChecked(el) ? obj(inputName(el), inputValue(el)) : {};
 }
+function serializeCheckbox(el) {
+  return isChecked(el) ? obj(inputName(el), true) : {};
+}
+function serializeFile(el) {
+  return {};
+}
+
+export const serializeInput = ifElse(
+  isDisabled,
+  always({}),
+  ifElse(
+    isFile,
+    serializeFile,
+    ifElse(
+      isCheckbox,
+      serializeCheckbox,
+      ifElse(
+        isRadio,
+        serializeRadio,
+        fn2(obj, inputName, inputValue)
+      )
+    )
+  )
+);
 
 function serializeFormReducer(acc, el) {
   return Object.assign({}, acc, serializeInput(el));
--- test/form.test.js
+++ test/form.test.js
@@ -5,30 +5,153 @@ const dom = require('..');
 
 describe('inputType()', function () {
 
-  it('should return lowercased input type');
+  it('should return lowercased input type', function () {
 
-  it('should return text as input type if element type is not defined');
+    const input = dom.h('input', {type: 'RADIO'}, {}, []);
+    const el = dom.hToDOM(input);
+
+    expect(dom.inputType(el)).to.equal('radio');
+
+  });
+
+  it('should return text as input type if element type is not defined', function () {
+
+    const el = dom.createElement('input');
+
+    expect(el.getAttribute('type')).to.be.null;
+    expect(dom.inputType(el)).to.equal('text');
+
+  });
 
 });
 
 
 describe('serializeInput()', function () {
 
-  it('should return object');
+  it('should return object', function () {
 
-  it('should return empty object for file input');
+    const input = dom.h('input', {name: 'foo', value: 'baz'}, {}, []);
+    const el = dom.hToDOM(input);
 
-  it('should return empty object for disabled input');
+    expect(dom.serializeInput(el)).to.be.an('object');
 
-  it('should return empty object for not selected boolean input');
+  });
 
-  it('should return serialized value');
+  it('should return empty object for file input', function () {
+
+    const input = dom.h('input', {type: 'file', name: 'foo', value: 'baz'}, {}, []);
+    const el = dom.hToDOM(input);
+
+    expect(dom.serializeInput(el)).to.deep.equal({});
+
+  });
+
+  it('should return empty object for disabled input', function () {
+
+    const input = dom.h('input', {type: 'text', name: 'foo', value: 'baz', disabled: 'disabled'}, {}, []);
+    const el = dom.hToDOM(input);
+
+    expect(dom.serializeInput(el)).to.deep.equal({});
+
+  });
+
+  it('should return empty object for not selected checkbox input', function () {
+
+    const input = dom.h('input', {type: 'checkbox', name: 'foo'}, {}, []);
+    const el = dom.hToDOM(input);
+
+    expect(dom.serializeInput(el)).to.deep.equal({});
+
+  });
+
+  it('should return empty object for not selected radio input', function () {
+
+    const input = dom.h('input', {type: 'radio', name: 'foo'}, {}, []);
+    const el = dom.hToDOM(input);
+
+    expect(dom.serializeInput(el)).to.deep.equal({});
+
+  });
+
+  it('should return serialized value for selected checkbox input', function () {
+
+    const input = dom.h('input', {type: 'checkbox', name: 'foo', checked: 'checked'}, {}, []);
+    const el = dom.hToDOM(input);
+
+    expect(dom.serializeInput(el)).to.deep.equal({foo: true});
+
+  });
+
+  it('should return serialized value for selected radio input', function () {
+
+    const input = dom.h('input', {type: 'radio', name: 'foo', checked: 'checked', value: 'baz'}, {}, []);
+    const el = dom.hToDOM(input);
+
+    expect(dom.serializeInput(el)).to.deep.equal({foo: 'baz'});
+
+  });
+
+  it('should return serialized value for text input', function () {
+
+    const input = dom.h('input', {type: 'text', name: 'foo', value: 'baz'}, {}, []);
+    const el = dom.hToDOM(input);
+
+    expect(dom.serializeInput(el)).to.deep.equal({foo: 'baz'});
+
+  });
+
+  it('should return serialized value for password input', function () {
+
+    const input = dom.h('input', {type: 'password', name: 'foo', value: 'baz'}, {}, []);
+    const el = dom.hToDOM(input);
+
+    expect(dom.serializeInput(el)).to.deep.equal({foo: 'baz'});
+
+  });
+
+  it('should return serialized value for hidden input', function () {
+
+    const input = dom.h('input', {type: 'hidden', name: 'foo', value: 'baz'}, {}, []);
+    const el = dom.hToDOM(input);
+
+    expect(dom.serializeInput(el)).to.deep.equal({foo: 'baz'});
+
+  });
 
 });
 
 
 describe('serializeForm()', function () {
 
-  it('should serialize all inputs with names');
+  it('should serialize all inputs with names', function () {
+
+    const tree = dom.h('form', {}, {}, [
+      dom.h('input', {type: 'text', name: 'aaa', value: ''}, {}, []),
+      dom.h('input', {type: 'text', name: 'bbb', value: 'bbb'}, {}, []),
+      dom.h('input', {type: 'password', name: 'ccc', value: 'ccc'}, {}, []),
+      dom.h('input', {type: 'hidden', name: 'ddd', value: 'ddd'}, {}, []),
+      dom.h('input', {type: 'file', name: 'eee', value: 'eee'}, {}, []),
+      dom.h('input', {type: 'checkbox', name: 'fff'}, {}, []),
+      dom.h('input', {type: 'checkbox', name: 'ggg', checked: 'checked'}, {}, []),
+      dom.h('input', {type: 'radio', name: 'hhh', value: 'h'}, {}, []),
+      dom.h('input', {type: 'radio', name: 'hhh', value: 'hh'}, {}, []),
+      dom.h('input', {type: 'radio', name: 'hhh', checked: 'checked', value: 'hhh'}, {}, []),
+      dom.h('input', {type: 'text', name: 'iii', value: 'iii', disabled: 'disabled'}, {}, []),
+      dom.h('input', {name: 'jjj', value: 'jjj'}, {}, []),
+    ]);
+    const el = dom.hToDOM(tree);
+    const expected = {
+      aaa: '',
+      bbb: 'bbb',
+      ccc: 'ccc',
+      ddd: 'ddd',
+      ggg: true,
+      hhh: 'hhh',
+      jjj: 'jjj'
+    };
+
+    expect(dom.serializeForm(el)).to.deep.equal(expected);
+
+  });
 
 });
